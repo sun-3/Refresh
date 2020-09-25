9b5c6aed29f6a132d2b50144550d7ac90581eff8
shldhll <iam.shldhll@gmail.com>
2020-09-25 11:10:38 +0530

    connected StartActivity with MainActivity

--- app/src/main/java/application/android/refresh/MainActivity.kt
+++ app/src/main/java/application/android/refresh/MainActivity.kt
@@ -1,10 +1,12 @@
 package application.android.refresh
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +17,12 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
+    }
+
+    fun signOut() {
+        FirebaseAuth.getInstance().signOut()
+        val startActivityIntent = Intent(applicationContext, StartActivity::class.java)
+        startActivity(startActivityIntent)
+        finish()
     }
 }
--- app/src/main/java/application/android/refresh/StartActivity.kt
+++ app/src/main/java/application/android/refresh/StartActivity.kt
@@ -3,14 +3,12 @@ package application.android.refresh
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
 import com.google.firebase.auth.FirebaseAuth
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-        val navController = findNavController(R.id.nav_start_host_fragment)
         val auth = FirebaseAuth.getInstance()
 
         if (auth.currentUser != null) {
